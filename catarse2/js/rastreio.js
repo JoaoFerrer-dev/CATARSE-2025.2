aa9e5c056be4a63b68518f6f5ff26c81194f2e04
João Ferreira <joaovictornota100@gmail.com>
2025-11-18 20:49:54 -0300

    Delete catarse2 directory

--- catarse2/js/rastreio.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // ====== ACESSIBILIDADE ======
-    const accessibilityContainer = document.getElementById('accessibility-container');
-    if (accessibilityContainer) {
-        accessibilityContainer.style.top = '70vh'; // 70% da altura da tela
-        accessibilityContainer.style.left = '';
-        accessibilityContainer.style.right = '20px';
-    }
-});
